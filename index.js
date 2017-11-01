2429ea809f8ba18acbd088f0cf7ce20350833a36
michellejames <shellyjames90@yahoo.com>
2017-11-01 15:07:13 -0400

    Homework, create Robots, Robot

--- index.js
+++ index.js
@@ -1,21 +1,102 @@
 const express = require('express'), 		//helps turn node into a server that handles http requests
-	bodyParser = require('body-parser'),
-	cors = require('cors'),
-	mongoose = require('mongoose'),
-	app = express();	
+	app = express(),
+	mongoose = require('mongoose');
 
-	app.use(cors())
-	app.use(bodyParser.urlencoded({extended: true}));
-	mongoose.connect('mongodb://localhost:4006');
+	mongoose.connect('mongodb://localhost/test');
+
+	app.get('/', function(req, res) {
+		res.send("Hello World");
+	})
+
+	var Robot = mongoose.model('Robot', { 
+		name: {type: String, required: true},
+		evil: Boolean,
+		movement: String,
+		literature: String,
+		catchphrases: [String],
+		url: String,
+	});
+
+	app.get('/robots', function(req, res) {			
+		
+		Robot.find({}, function(err, robots) {			//find all the robots
+			console.log(err, robots);
+
+			if (err) {
+				res.send(500, "Server Error")
+			} else if (!robots){
+				res.send(400, "Couldn't find robots!")
+			} else {
+				res.json(robots);
+			}
+
+		})		
+
+		Robot.find({url: "wallee"}, function(err, robots) {
+			console.log(err, robots);
+		})		
+	})
+
+	app.get('/robots/:url', function(req, res) {			
+		
+		Robot.findOne({url: req.params.url}, function(err, robot) {			//find ONE the robots
+			console.log(err, robot);
+			
+			if (err) {
+				res.send(500, "Server Error")
+			} else if (!robot){
+				res.send(400, "Couldn't find the robot!")
+			} else {
+				res.json(robot);
+			}
+		})	
+	})
 
 
-	//GET 		/item 			get all the items		(read)
-	//POST 		/item 			add a new item 			(create)
-	//GET 		/item/1 		get item 1 				(read)
-	//POST 		/item/1			edit item 1 			(update)
-	//DELETE 	/item/1			delete item 1 			(delete)
+	app.get('/seed', function(req, res) {
 
-	//GET 		/item1/comment	get comments for item 1	(read)
+		var roboKid = new Robot({ 
+			name: 'roboKid',
+			evil: true,
+			movement: "fast",
+			literature: "Shelly's RoboWorld",
+			catchphrases: [
+				"Oil me up",
+				"Trash Talk",
+				],
+			url: "robokid"
+		});
+
+		roboKid.save(function (err) {
+		  if (err) {
+		    console.log(err);
+		  } else {
+		    console.log(roboKid.catchphrases[0]);
+		  }
+		});
+
+		var wallee = new Robot({ 
+			name: 'walle-E',
+			evil: null,
+			movement: "slow",
+			literature: "walle-E",
+			catchphrases: [
+				"wall-eeeeeee",
+				"Evaaaaaaaa",
+				],
+			url: "wallee"
+		});
+
+		wallee.save(function (err) {
+		  if (err) {
+		    console.log(err);
+		  } else {
+		    console.log(wallee.catchphrases[1]);
+		  }
+		});
+
+		res.send("Seeded!");
+	})
 
 
 
